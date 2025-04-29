4ebd9a7739f507184b682e096abd607e468844be
amanda-bea <amanda040a@gmail.com>
2025-04-28 20:34:20 -0400

    estrutura inicial corrigida

---
+++ Sistema/src/main/java/com/ufms/eventos/controller/EventoController.java
@@ -0,0 +1,21 @@
+package com.ufms.eventos.controller;
+
+import com.ufms.eventos.model.Evento;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventoController {
+    private List<Evento> eventos;
+
+    public EventoController() {
+        this.eventos = new ArrayList<>();
+    }
+
+    public void adicionarEvento(Evento evento) {
+        eventos.add(evento);
+    }
+
+    public List<Evento> listarEventos() {
+        return eventos;
+    }
+}
---
+++ Sistema/src/main/java/com/ufms/eventos/view/MainView.java
@@ -0,0 +1,46 @@
+package com.ufms.eventos.view;
+
+import com.ufms.eventos.controller.EventoController;
+import com.ufms.eventos.model.Evento;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+    private EventoController controller = new EventoController();
+
+    @Override
+    public void start(Stage primaryStage) {
+        VBox root = new VBox();
+
+        TextField nomeField = new TextField();
+        nomeField.setPromptText("Nome do Evento");
+
+        TextField dataField = new TextField();
+        dataField.setPromptText("Data do Evento");
+
+        Button adicionarButton = new Button("Adicionar Evento");
+        adicionarButton.setOnAction(e -> {
+            String nome = nomeField.getText();
+            String data = dataField.getText();
+            controller.adicionarEvento(new Evento(nome, data));
+            nomeField.clear();
+            dataField.clear();
+        });
+
+        root.getChildren().addAll(nomeField, dataField, adicionarButton);
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Gerenciador de Eventos");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
